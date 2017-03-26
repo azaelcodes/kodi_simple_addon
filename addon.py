191008e21b4a1ab5559d274566a6ede9b63cfd1e
azaelgarcia <azael.garcia@flighthub.com>
2017-03-26 14:29:18 -0400

    More testing

--- addon.py
+++ addon.py
@@ -1,14 +1,13 @@
 import xbmc, xbmcgui, xbmcaddon
 
-addonPath = xbmc.translatePath(Addon.getAddonInfo("path")).decode("utf-8")
-libDir = os.path.join(addonPath, 'resources', 'lib')
-sys.path.insert(0, libDir)
-import common
-
 addon = xbmcaddon.Addon('plugin.video.azaelcodesfavs')
 title = addon.getAddonInfo('name')
 icon = addon.getAddonInfo('icon')
 user_dataDir = xbmc.translatePath(addon.getAddonInfo("profile")).decode("utf-8")
+addonPath = xbmc.translatePath(Addon.getAddonInfo("path")).decode("utf-8")
+libDir = os.path.join(addonPath, 'resources', 'lib')
+sys.path.insert(0, libDir)
+import common
 
 FAVORITES = os.path.join(user_dataDir, 'favorites.txt')
 if not (os.path.isfile(FAVORITES)):
