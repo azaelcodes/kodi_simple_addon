111b397ced6add9180cad9eb8f3c2cfacb392761
azaelgarcia <azael.garcia@flighthub.com>
2017-03-26 16:16:13 -0400

    Testing

--- addon.py
+++ addon.py
@@ -6,9 +6,5 @@ title = addon.getAddonInfo('name')
 icon = addon.getAddonInfo('icon')
 user_dataDir = xbmc.translatePath(addon.getAddonInfo("profile")).decode("utf-8")
 
-# List example
-addonHandle = int(sys.argv[1])
-li = xbmcgui.ListItem(user_dataDir, iconImage='none.png')
-xbmcplugin.addDirectory(handle=addonHandle, 'url:url//', listitem=li)
-
-xbmcplugin.endOfDirectory(addonHandle)
+# Data Directory
+xbmcgui.Dialog().ok(addon, 'Title 1', user_dataDir, 'Last one')
