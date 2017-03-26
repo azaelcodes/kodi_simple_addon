a672c8d9c04eac0126c708e4116979ad11a742d5
azaelgarcia <azael.garcia@flighthub.com>
2017-03-26 16:27:08 -0400

    More code for list item

--- addon.py
+++ addon.py
@@ -1,6 +1,11 @@
 import sys
 import xbmc, xbmcgui, xbmcaddon, xbmcplugin
 
+# Get the plugin url in plugin:// notation.
+_url = sys.argv[0]
+# Get the plugin handle as an integer number.
+_handle = int(sys.argv[1])
+
 addon = xbmcaddon.Addon('plugin.video.azaelcodesfavs')
 title = addon.getAddonInfo('name')
 icon = addon.getAddonInfo('icon')
@@ -8,3 +13,5 @@ user_dataDir = xbmc.translatePath(addon.getAddonInfo("profile")).decode("utf-8")
 
 # Data Directory
 list_item = xbmcgui.ListItem(label=user_dataDir)
+xbmcplugin.addDirectoryItem(_handle, url, list_item, false)
+xbmcplugin.endOfDirectory(_handle)
