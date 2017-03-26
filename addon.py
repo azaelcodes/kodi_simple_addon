965e8a4376f6cd105aadd43bd1eaa1b7e7a3b9e8
azaelgarcia <azael.garcia@flighthub.com>
2017-03-26 17:05:34 -0400

    Added another item

--- addon.py
+++ addon.py
@@ -10,8 +10,10 @@ addon = xbmcaddon.Addon('plugin.video.azaelcodesfavs')
 title = addon.getAddonInfo('name')
 icon = addon.getAddonInfo('icon')
 user_dataDir = xbmc.translatePath(addon.getAddonInfo("profile")).decode("utf-8")
+user_addonData = os.path.join(xbmc.translatePath('special://userdata'))
 
 # Data Directory
 list_item = xbmcgui.ListItem(label=user_dataDir)
+xbmcplugin.ListItem(label=user_addonData)
 xbmcplugin.addDirectoryItem(_handle, _url, list_item, False)
 xbmcplugin.endOfDirectory(_handle)
