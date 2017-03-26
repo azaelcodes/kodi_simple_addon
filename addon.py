15c84e59cedd134bfb7553c79273efd698045868
azaelgarcia <azael.garcia@flighthub.com>
2017-03-26 14:40:04 -0400

    Added method to write to a list

--- addon.py
+++ addon.py
@@ -1,14 +1,29 @@
 import xbmc, xbmcgui, xbmcaddon
+import os, io, json
 
 addon = xbmcaddon.Addon('plugin.video.azaelcodesfavs')
 title = addon.getAddonInfo('name')
 icon = addon.getAddonInfo('icon')
 user_dataDir = xbmc.translatePath(addon.getAddonInfo("profile")).decode("utf-8")
-import common
+
+def write_list(fileName, list, indent=True):
+    try:
+        with io.open(fileName, 'w', encoding='utf-8') as handle:
+            if indent:
+                handle.write(unicode(json.dumps(list, indent=2, ensure_ascii=False)))
+            else:
+                handle.write(unicode(json.dumps(list, ensure_ascii=False)))
+        success = True
+    except Exception as ex:
+        xbmc.log("{0}".format(ex), 3)
+        success = False
+
+    return success
+
 
 FAVORITES = os.path.join(user_dataDir, 'favorites.txt')
 if not (os.path.isfile(FAVORITES)):
-    common.WriteList(FAV, 'Hello-World')
+    write_list(FAV, 'Hello-World')
 xbmcgui.Dialog().ok(title, 'Hello', 'World', 'Azael')
 
 
