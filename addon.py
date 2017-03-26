480e70ffd29bc57bd1b5a6177ec9d7e97691b08b
azaelgarcia <azael.garcia@flighthub.com>
2017-03-26 14:31:14 -0400

    Still testing this kodi thing

--- addon.py
+++ addon.py
@@ -4,9 +4,6 @@ addon = xbmcaddon.Addon('plugin.video.azaelcodesfavs')
 title = addon.getAddonInfo('name')
 icon = addon.getAddonInfo('icon')
 user_dataDir = xbmc.translatePath(addon.getAddonInfo("profile")).decode("utf-8")
-addonPath = xbmc.translatePath(Addon.getAddonInfo("path")).decode("utf-8")
-libDir = os.path.join(addonPath, 'resources', 'lib')
-sys.path.insert(0, libDir)
 import common
 
 FAVORITES = os.path.join(user_dataDir, 'favorites.txt')
